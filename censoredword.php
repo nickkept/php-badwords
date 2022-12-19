b0023cb6bb93f1b47b371157f85c37ce8954ff56
nickkept <nickit1899@gmail.com>
2022-12-19 17:32:21 +0100

    finished project

---
+++ censoredword.php
@@ -0,0 +1,29 @@
+<?php
+$getSentence = $_GET["sentence"];
+$getCensored = $_GET["censored"];
+
+echo $getSentence ."<br>";
+echo $getCensored ."<br>";
+$censoredSentence = str_replace("sushi", "*****", $getSentence);
+$sentenceLenght = strlen($getSentence);
+echo $sentenceLenght;
+echo $censoredSentence;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Bad Words</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+</head>
+<body class="bg-dark">
+    <div class="container text-danger text-center">
+        <h1> Full Sentence: <span class="text-white"> <?php echo $getSentence ?></span> </h1>
+        <h2> Sentence lenght: <span class="text-white"> <?php echo $sentenceLenght?></span></h2>
+        <h3> Censored Sentence: <span class="text-white"> <?php echo $censoredSentence ?></span> </h3>
+    </div>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Bad Words</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css">
+</head>
+<body class="bg-dark">
+    <div class="container text-white">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <form action="censoredword.php" method="GET">
+                    <legend class="fw-bold">Contatti [GET]</legend>
+                    <div class="my-3">
+                        <label for="" class="form-label fw-bold">Write a sentence:</label>
+                        <input type="text" class="form-control" name="sentence">
+                    </div>
+                    <div class="my-2">
+                        <label for="" class="form-label fw-bold">Bad Words:</label>
+                        <input type="text" class="form-control" name="censored">
+                    </div>
+                    
+                        <button class="btn btn-light text-center">Send</button>
+                    
+                </form>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
